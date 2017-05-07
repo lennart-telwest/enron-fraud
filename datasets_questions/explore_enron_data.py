943eb4cedaf5e2c5b850d5473582260d73bbd2e7
Lennart Telwest <ltelwest@googlemail.com>
2017-05-07 22:22:23 +0200

    explore the dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -22,16 +22,37 @@ enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r")
 num_persons = 0
 num_features = 0
 num_pois = 0
-
-for key in enron_data.iterkeys():
-    print key
-
+num_persons_with_salary = 0
+num_persons_with_mail = 0
+num_persons_with_total_payments = 0
+num_pois_with_total_payments = 0
+# for key in enron_data.iterkeys():
+# 	for feature in enron_data[key]:
+# 		num_features += 1
+# 	break
+# print num_features
 #print enron_data["PRENTICE JAMES"]["total_stock_value"]
 
-# for key in enron_data.iterkeys():
-#     if enron_data[key]["poi"] == 1:
-#         num_pois += 1
-# print num_pois
+for key in enron_data.iterkeys():
+	if enron_data[key]["poi"] == 1:
+		num_pois += 1
+	if enron_data[key]["salary"] != 'NaN':
+		num_persons_with_salary += 1
+	if enron_data[key]["email_address"] != 'NaN':
+ 		num_persons_with_mail += 1
+	if enron_data[key]["total_payments"] != 'NaN':
+		num_persons_with_total_payments += 1
+	if enron_data[key]["total_payments"] != 'NaN' and enron_data[key]["poi"] == 1 == 1:
+		num_pois_with_total_payments += 1
+ 	num_persons += 1
+
+print 'total number of persons:', num_persons
+print 'num_pois:', num_pois
+print 'persons with salary:', num_persons_with_salary
+print 'persons with mail:', num_persons_with_mail
+print 'num_persons_with_total_payments', num_persons_with_total_payments
+print 'percentage of persons w total payments:', num_persons_with_total_payments*1.0/num_persons
+print 'percentage pois with total_payments:', num_pois_with_total_payments*1.0/num_pois
 
 # for person in enron_data:
 #     num_persons += 1 
