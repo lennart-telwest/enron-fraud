b010dfb18fe748cac6dbc9b64db7b64d638a0f01
Lennart Telwest <ltelwest@googlemail.com>
2017-08-21 18:41:12 +0200

    bag of words

--- try/bag_of_words.py
+++ try/bag_of_words.py
@@ -1,7 +1,11 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
-string1 = "hi Katie"
+vectorizer = CountVectorizer()
+string1 = "Hi Katie"
 string2 = "Hi sebastian"
 string3 = "Hi Katie"
 
 email_list = [string1, string2, string3]
+bag_of_words = vectorizer.fit(email_list)
+bag_of_words = vectorizer.transform(email_list)
+print(bag_of_words)
