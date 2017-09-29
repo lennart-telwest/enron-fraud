81ca8416b97bb0ce4a97addbfd72338ea6ffe1ff
Lennart Telwest <ltelwest@googlemail.com>
2017-09-29 13:39:52 +0200

    add adjustments

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,20 +2,45 @@
 
 import sys
 import pickle
+import numpy as np
+from pandas import DataFrame
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
-### Task 1: Select what features you'll use.
+### Task 1: Selecting the features that will be used.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+# email data
+features_email = ['to_messages', 'from_messages',  'from_poi_to_this_person',
+           'from_this_person_to_poi', 'shared_receipt_with_poi']
+# finance data
+features_finance = ['salary', 'bonus', 'long_term_incentive', 'deferred_income',
+             'deferral_payments', 'loan_advances', 'other', 'expenses',
+             'director_fees', 'total_payments',
+             'exercised_stock_options', 'restricted_stock',
+             'restricted_stock_deferred', 'total_stock_value']
+# all features
+features_list = features_email + features_finance
+# all features column names
+features_column_names = ['poi'] + ['email_address'] + features_email + features_finance
+# all features data type
+features_dtype = [bool] + [str] + list(np.repeat(float, 19))
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
-
+# converting the data into a data frame
+df = DataFrame.from_dict(data_dict, orient='index')
+# reordering the columns
+df = df.ix[:, features_column_names]
+# converting the data type
+for i in xrange(len(features_column_names)):
+    df[features_column_names[i]] = df[features_column_names[i]].astype(features_dtype[i], errors='ignore')
+# Replace missing data with 0
+for f in features_finance:
+    df.loc[df[f].isnull(), f] = 0
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
@@ -35,15 +60,15 @@ labels, features = targetFeatureSplit(data)
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
