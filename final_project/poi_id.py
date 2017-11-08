fd3f1b54c2f6110058f33735ab78a64c38f7abd7
Lennart Telwest <lennart@telwest.org>
2017-11-08 11:24:14 +0100

    add final eval script

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -3,6 +3,7 @@
 import sys
 import pickle
 import numpy as np
+from numpy import mean
 import pandas as pd
 from pandas import DataFrame
 from sklearn.pipeline import make_pipeline
@@ -10,15 +11,19 @@ from sklearn.preprocessing import Imputer
 from sklearn.ensemble import ExtraTreesClassifier
 import seaborn as sns
 import matplotlib.pyplot as plt
-from time import time
 from sklearn.feature_selection import SelectKBest, f_classif, SelectFpr
 from sklearn.decomposition import RandomizedPCA
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn import cross_validation
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
-from sklearn.cross_validation import train_test_split
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -32,7 +37,6 @@ features_finance = ['salary', 'bonus', 'long_term_incentive', 'deferred_income',
              'director_fees', 'total_payments',
              'exercised_stock_options', 'restricted_stock',
              'restricted_stock_deferred', 'total_stock_value']
-
 # all features
 features_list = features_email + features_finance
 # all features column names
@@ -54,34 +58,16 @@ df = df.loc[:, features_column_names]
 for i in xrange(len(features_column_names)):
     df[features_column_names[i]] = df[features_column_names[i]].astype(features_dtype[i], errors='ignore')
 
-# Assign 0 to NaN in finance features
-for f in features_finance:
-    df.loc[df[f].isnull(), f] = 0
-
 ### Task 2: Remove outliers
 df = df[df.index != 'TOTAL']
 df = df[df.index != 'THE TRAVEL AGENCY IN THE PARK']
 
-b_robert = data_dict["BELFER ROBERT"]
-
-b_robert['deferred_income'] = 102500
-b_robert['deferral_payments']= 'NaN'
-b_robert['expenses'] = 3285
-b_robert['directors_fees'] = 102500
-b_robert['total_payments']=3285
-b_robert['exercised_stock_options']='NaN'
-b_robert['restricted_stock_options']=44093
-
-b_sandjay = data_dict['BHATNAGAR SANJAY']
-
-b_sandjay['expenses'] = 137864
-b_sandjay['total_payments'] = 137864
-b_sandjay['exercised_stock_options'] = 15456734
-b_sandjay['restricted_stock'] = 2604490
-b_sandjay['restricted_stock_deferred'] = 2604490
-b_sandjay['total_stock_value'] = 15456290
-b_sandjay['director_fees'] = 'NaN'
-b_sandjay['other'] = 'NaN'
+df.loc['BELFER ROBERT', features_finance] = \
+    [0, 0, 0, -102500, 0, 0, 0, 3285,
+     102500, 3285, 0, 44093, -44093, 0]
+df.loc['BHATNAGAR SANJAY', features_finance] = \
+    [0, 0, 0, 0, 0, 0, 0, 137864, 0, 137864,
+     15456290, 2604490, -2604490, 15456290]
 
 ### Task 3: Create new feature(s)
 # calculate ratio
@@ -92,15 +78,38 @@ df['shared_receipt_with_poi_ratio'] = df['shared_receipt_with_poi'] / df['to_mes
 features_email_new = ['recieved_from_poi_ratio', 'sent_to_poi_ratio', 'shared_receipt_with_poi_ratio']
 features_all = features_list + features_email_new
 
-# log scaling of all features
-for f in features_all:
+# log scaling
+for f in features_list:
     df[f] = [np.log(abs(v)) if v != 0 else 0 for v in df[f]]
+# replace null values
+df.replace(0, np.NaN)
+df.fillna(df.mean(), inplace=True)
+
+### Store to my_dataset for easy export below.
+my_dataset = df.to_dict(orient='index')
+
+
+### Extract features and labels from dataset for local testing
+
+#Select the labels
+my_feature_list = ['poi'] + features_email + features_finance
+# my_feature_list = ['poi'] + ['sent_to_poi_ratio'] + ['restricted_stock'] + ['exercised_stock_options'] + ['from_poi_to_this_person'] + ['shared_receipt_with_poi'] + ['bonus'] + ['long_term_incentive']
+# print my_feature_list[1]
+
+
+# Format and splot to labels and features
+data = featureFormat(my_dataset, my_feature_list, remove_NaN=True, sort_keys = False)
+labels, features = targetFeatureSplit(data)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=2000,gamma = 0.0001,random_state = 42, class_weight = 'auto')
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
@@ -109,62 +118,38 @@ for f in features_all:
 ### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-#pipe = make_pipeline(
-#          Imputer(axis=0, copy=True, missing_values='NaN',
-#                  strategy='median', verbose=0),
-#          PCA(copy=True, n_components=12, whiten=True),
-#          LogisticRegression(C=1, class_weight='balanced', dual=False,
-#                             fit_intercept=True, intercept_scaling=0.6,
-#                             max_iter=100, multi_class='ovr', n_jobs=-1,
-#                             penalty='l2', random_state=None,
-#                             solver='liblinear', tol=0.0001, verbose=0,
-#                             warm_start=False))
-#pipe = make_pipeline(
-#          Imputer(axis=0, copy=True, missing_values='NaN',
-#                  strategy='median', verbose=0),
-#          SVC(C=1, cache_size=200, class_weight='balanced', coef0=0.0,
-#              decision_function_shape='ovo', degree=3, gamma='auto',
-#              kernel='linear', max_iter=-1, probability=False,
-#              random_state=20160308, shrinking=False, tol=0.001,
-#              verbose=False))
-#pipe = make_pipeline(
-#          Imputer(axis=0, copy=True, missing_values='NaN',
-#                  strategy='median', verbose=0),
-#          PCA(copy=True, n_components=18, whiten=True),
-#          SVC(C=1, cache_size=200, class_weight='balanced', coef0=0.0,
-#              decision_function_shape='ovo', degree=3, gamma='auto',
-#              kernel='linear', max_iter=-1, probability=False,
-#              random_state=20160308, shrinking=False, tol=0.001,
-#              verbose=False))
-pipe = make_pipeline(
-          Imputer(axis=0, copy=True, missing_values='NaN',
-                  strategy='median', verbose=0),
-          ExtraTreesClassifier(bootstrap=False, class_weight='balanced',
-                               criterion='gini', max_depth=None,
-                               max_features='sqrt', max_leaf_nodes=None,
-                               min_samples_leaf=3, min_samples_split=2,
-                               min_weight_fraction_leaf=0.0, n_estimators=30,
-                               n_jobs=-1, oob_score=False,
-                               random_state=20160308, verbose=0,
-                               warm_start=False))
-#pipe = make_pipeline(
-#          Imputer(axis=0, copy=True, missing_values='NaN',
-#                  strategy='median', verbose=0),
-#          SelectFpr(alpha=0.05, score_func=f_classif),
-#          ExtraTreesClassifier(bootstrap=False, class_weight='balanced',
-#                               criterion='gini', max_depth=None,
-#                               max_features='sqrt', max_leaf_nodes=None,
-#                               min_samples_leaf=3, min_samples_split=2,
-#                               min_weight_fraction_leaf=0.0, n_estimators=30,
-#                               n_jobs=-1, oob_score=False,
-#                               random_state=20160308, verbose=0,
-#                               warm_start=False))
+# Example starting point. Try investigating other evaluation techniques!
 
+def evaluate_clf(clf, features, labels, num_iters=1000, test_size=0.3):
+    print clf
+    accuracy = []
+    precision = []
+    recall = []
+    first = True
+    for trial in range(num_iters):
+        features_train, features_test, labels_train, labels_test =\
+            cross_validation.train_test_split(features, labels, test_size=test_size)
+        clf.fit(features_train, labels_train)
+        predictions = clf.predict(features_test)
+        accuracy.append(accuracy_score(labels_test, predictions))
+        precision.append(precision_score(labels_test, predictions))
+        recall.append(recall_score(labels_test, predictions))
+        if trial % 10 == 0:
+            if first:
+                sys.stdout.write('\nProcessing')
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            first = False
 
+    print "done.\n"
+    print "precision: {}".format(mean(precision))
+    print "recall:    {}".format(mean(recall))
+    return mean(precision), mean(recall)
 
+evaluate_clf(clf, features, labels)
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(pipe, df.to_dict(orient='index'), ['poi'] + features_all)
+dump_classifier_and_data(clf, my_dataset, features_list)
